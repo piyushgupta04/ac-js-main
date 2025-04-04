4b2197f1034d74ec72ce3f7d5c33bf4ab1824594
piyushgupta04 <normipiyush69@gmail.com>
2025-04-04 22:04:48 +0530

    added today's work
    not too much but ok

---
+++ live practice pages/script.js
@@ -0,0 +1,23 @@
+const h3 = document.getElementsByTagName("h3")
+// alert("Hello Codersssss!")
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// Window Object
+console.log(window)
+console.log("Hello")
+window.console.log("Hello")
+
+// We have something called console.dir, ye basically kissi bhi oject ki properties print karwa deta ha (check more on mdn reference!)
+
+// document.body.style.background = "red"
+let myH3 = document.getElementById("myH3")
+console.dir(myH3)
+let myPs = document.getElementsByClassName("my-p")
+console.dir(myPs)
+console.log(myPs)
+
+// document.querySelector
+
+// document.querySelectorAll
