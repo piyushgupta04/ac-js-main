9e962f0b4ea97cfe5609a077c318b3fed08a5a76
piyushgupta04 <normipiyush69@gmail.com>
2025-04-06 23:03:15 +0530

    *

--- live practice pages/script.js
+++ live practice pages/script.js
@@ -1,23 +1,42 @@
-const h3 = document.getElementsByTagName("h3")
-// alert("Hello Codersssss!")
-// document.getElementById
-// document.getElementsByClassName
-// document.getElementsByTagName
+// const h3 = document.getElementsByTagName("h3")
+// * alert("Hello Codersssss!")
+// * document.getElementById
+// * document.getElementsByClassName
+// * document.getElementsByTagName
 
-// Window Object
-console.log(window)
-console.log("Hello")
-window.console.log("Hello")
+// * Window Object
+// console.log(window)
+// console.log("Hello")
+// window.console.log("Hello")
 
-// We have something called console.dir, ye basically kissi bhi oject ki properties print karwa deta ha (check more on mdn reference!)
+// * We have something called console.dir, ye basically kissi bhi oject ki properties print karwa deta ha (check more on mdn reference!)
 
-// document.body.style.background = "red"
-let myH3 = document.getElementById("myH3")
-console.dir(myH3)
-let myPs = document.getElementsByClassName("my-p")
-console.dir(myPs)
-console.log(myPs)
+// * document.body.style.background = "red"
+// let myH3 = document.getElementById("myH3")
+// console.dir(myH3)
+// let myPs = document.getElementsByClassName("my-p")
+// console.dir(myPs)
+// console.log(myPs)
 
-// document.querySelector
+// * document.querySelector
+// * document.querySelectorAll
 
-// document.querySelectorAll
+// ! DOM MANIPULATION
+/* 
+* tagName
+* innerText
+* innerHTML
+* textContent
+*/
+
+// ! --------------------------------------------
+// * accessing html in js
+// console.dir(window.document.body);
+console.dir(document.body);
+console.log(document.body);
+// window glbal object ha issi liye usko likhna na likna sab barabar ha.
+
+let myP = document.getElementById("my-p")
+console.dir(myP)
+
+// * Continue from: https://youtu.be/7zcXPCt8Ck0?si=OtmmXS0XMl5sfNd3&t=2605
