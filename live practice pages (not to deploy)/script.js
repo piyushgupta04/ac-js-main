1e895691f5d420daaf57eb8132a8c0fe139edb36
piyushgupta04 <normipiyush69@gmail.com>
2025-04-08 22:58:07 +0530

    completed lecture 6 FR
    ek pq baccha ha bass!

--- live practice pages (not to deploy)/script.js
+++ live practice pages (not to deploy)/script.js
@@ -36,7 +36,56 @@ console.dir(document.body);
 console.log(document.body);
 // window glbal object ha issi liye usko likhna na likna sab barabar ha.
 
-let myP = document.getElementById("my-p")
-console.dir(myP)
+let myPs = document.getElementsByClassName("my-p")
+console.dir(myPs)
+console.log(myPs)
+console.log("HTML COLLECTION IS VERY SIMILAR TO ARRAY")
 
-// * Continue from: https://youtu.be/7zcXPCt8Ck0?si=OtmmXS0XMl5sfNd3&t=2605
+// ! Continue from: https://youtu.be/7zcXPCt8Ck0?si=OtmmXS0XMl5sfNd3&t=2605
+
+// using tagname
+let parahs = document.getElementsByTagName("p")
+console.dir(parahs)
+
+// Query selector
+let elements = document.querySelector("p") // 1st element
+console.dir(elements)
+
+// ! DOM MANIPULATION
+/* 
+* tagName
+* innerText
+* innerHTML
+* textContent
+*/
+
+// MDN fist last child sibling [ text, comment, element ]
+
+// tagName
+let f1Btn = document.getElementById("myBtn");
+console.dir(f1Btn)
+
+let myDiv = document.querySelector(".my-div");
+console.dir(myDiv)
+// innerText
+// innerHTML
+// script.js:35 body
+// myDiv.innerHTML = "<div> Hello World, from Console </div>"
+// Output: '<div> Hello World, from Console </div>'
+// innerContent | ye basically jo cheese hidden hoti ha html pe ye unko bhi print karra det ha, innerText jaisa hi work karta ha but hidden contents bhi print karra deta ha!
+
+
+// ! Practice ...
+let h2 = document.querySelector("h2")
+h2.innerText = h2.innerText + " from Aapna College"
+
+let boxes = document.getElementsByClassName("box")
+console.dir(boxes)
+// boxes[0].innerHTML = "<i>Hello</i>"
+// boxes[1].innerHTML = "<b>Hello</b>"
+// boxes[2].innerHTML = "<strong>Hello</strong>"
+
+// yahi same kaam me code ke through bhi kar sakta hu
+// ! yaha code revise karke loop bana ke karo yahi same cheez
+// lecture khatam!
+// lecture 6 khatam!, bass ye practice wala revise karke karlo
